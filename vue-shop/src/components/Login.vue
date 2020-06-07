9a748dbfc70110d952366cfc330ad562af708b66
linyuan <dreamingclear@163.com>
2020-06-07 19:24:13 +0800

    [全局样式表引入,登录logo设置]

--- vue-shop/src/components/Login.vue
+++ vue-shop/src/components/Login.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    登录组件
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,4 +13,38 @@ export default {}
 </script>
 
 <style scoped>
+
+.login_container {
+  background: #2b4b6b;
+  height: 100%;
+}
+.login_box{
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+}
+
+.avatar_box{
+  height: 130px;
+  width: 130px;
+  border: solid 1px #eeeeee;
+  border-radius: 50%;
+  padding: 10px;
+  box-shadow: 0 0 10px;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  background-color: #fff;
+}
+.avatar_box img{
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: #eeeeee;
+}
 </style>>
--- vue-shop/src/main.js
+++ vue-shop/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 导入全局样式表
+import './assets/css/global.css'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 new Vue({
