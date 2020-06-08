a47254afec382d6808dedd83441ba1e00d11bd79
Qian <dreamingclear@163.com>
2020-06-08 17:29:28 +0800

    [home头部]

--- vue-shop/src/components/Home.vue
+++ vue-shop/src/components/Home.vue
@@ -2,7 +2,10 @@
   <el-container class="home_container">
     <!-- 头部去 -->
     <el-header>
-      Header
+      <div>
+        <img src="../assets/home_logo.png" />
+        <span>电商后台管理系统</span>
+      </div>
       <el-button type="info" @click="logoutAction">退出</el-button>
     </el-header>
     <!-- 页面主体区 -->
@@ -34,7 +37,19 @@ export default {
 .el-header {
   background-color: #373d41;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #ffffff;
+  font-size: 20px;
+  font-weight: 700;
+}
+.el-header div {
+  display: flex;
+  align-items: center;
+}
+span {
+  margin-left: 15px;
 }
 .el-aside {
   background-color: #333744;
