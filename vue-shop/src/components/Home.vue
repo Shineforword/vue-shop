310035b4809cb04d295ec2d5301268203f49e017
Qian <dreamingclear@163.com>
2020-06-08 19:18:27 +0800

    [添加图标]

--- vue-shop/src/components/Home.vue
+++ vue-shop/src/components/Home.vue
@@ -13,20 +13,20 @@
       <!-- 侧边栏 -->
       <el-aside width="200px">
         <!-- 侧边栏菜单区 -->
-        <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409eff">
           <!-- 一级菜单 -->
           <el-submenu :index="item.id+''" v-for="item in menuList" :key="item.id">
             <!-- 一级菜单模板 -->
             <template slot="title">
               <!-- 一级菜单图标 -->
-              <i class="el-icon-location"></i>
+              <i :class="iconsObject[item.id]"></i>
               <span>{{item.authName}}</span>
             </template>
             <!-- 二级菜单 -->
             <el-menu-item :index="subItem.id+''" v-for="subItem in item.children" :key="subItem.id">
               <template slot="title">
                 <!-- 菜单图标 -->
-                <i class="el-icon-location"></i>
+                <i class="el-icon-menu"></i>
                 <span>{{subItem.authName}}</span>
               </template>
             </el-menu-item>
@@ -43,7 +43,14 @@
 export default {
   data() {
     return {
-      menuList: []
+      menuList: [],
+      iconsObject: {
+        '125': 'iconfont icon-users',
+        '103': 'iconfont icon-tijikongjian',
+        '101': 'iconfont icon-lock_fill',
+        '102': 'iconfont icon-danju',
+        '145': 'iconfont icon-baobiao'
+      }
     }
   },
   // 生命周期函数
@@ -93,5 +100,8 @@ span {
 }
 .el-main {
   background-color: #eaedf1;
+}
+.iconfont {
+  margin-right: 10px;
 }
 </style>
