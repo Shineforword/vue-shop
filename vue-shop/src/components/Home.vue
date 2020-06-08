66c790fc9403477b6ab7733464b06ebe9ba25a22
Qian <dreamingclear@163.com>
2020-06-08 17:17:07 +0800

    [home页面主体布局]

--- vue-shop/src/components/Home.vue
+++ vue-shop/src/components/Home.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="home">
-    <el-button type="info" @click="logoutAction">退出</el-button>
-  </div>
+  <el-container class="home_container">
+    <!-- 头部去 -->
+    <el-header>
+      Header
+      <el-button type="info" @click="logoutAction">退出</el-button>
+    </el-header>
+    <!-- 页面主体区 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -15,4 +26,20 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.home_container {
+  width: 100%;
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: center;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
+</style>
