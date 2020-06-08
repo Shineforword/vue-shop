1031e8fdccf9e9134c4195d657a4d9464dbb1322
Qian <dreamingclear@163.com>
2020-06-08 16:41:13 +0800

    [退出登录(清空sessionStorage)]

--- vue-shop/src/components/Home.vue
+++ vue-shop/src/components/Home.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="home">
-    home组件
+    <el-button type="info" @click="logoutAction">退出</el-button>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    logoutAction() {
+      window.sessionStorage.clear()
+      this.$router.push('login')
+    }
+  }
+}
 </script>
 
 <style scoped></style>
--- vue-shop/src/router/index.js
+++ vue-shop/src/router/index.js
@@ -26,8 +26,7 @@ router.beforeEach((to, from, next) => {
   // to 表示将要访问的路径
   // from 代表从哪一个路径来
   // next 是一个函数表示放行
-  if (to.path === '/login') return
-  next()
+  if (to.path === '/login') return next()
   // 获取token
   const token = window.sessionStorage.getItem('token')
   if (!token) return next('/login')
